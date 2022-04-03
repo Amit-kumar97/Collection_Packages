925697bd267f32f36caf91054a8834339d18f6c4
Amit-rajput97 <84862054+Amit-rajput97@users.noreply.github.com>
2022-04-03 23:34:15 +0530

    Array operations

--- src/Arrays/Array_Operation.java
+++ src/Arrays/Array_Operation.java
@@ -1,15 +1,13 @@
 package Arrays;
 
+import java.util.Arrays;
+
 public class Array_Operation<T extends Comparable<? super T>> {
     T[] ar;
 
-    public Array_Operation(int size)
+    void insert(T values,int size)
     {
         ar = (T[]) new Array_Operation[size];
-    }
-
-    void insert(T values)
-    {
         if(ar.length==0)
             return;
 
@@ -36,5 +34,33 @@ public class Array_Operation<T extends Comparable<? super T>> {
             }
             return ar[temp];
         }
+    }
+    void mergeArray(T[] ar1, T[] ar2)
+    {
+        int size = ar1.length+ ar2.length;
+        ar = (T[])new Array_Operation[size];
+        for (int i = 0; i< ar1.length; i++)
+        {
+            ar[i] = ar1[i];
+        }
+        for (int i = 0; i < ar2.length ; i++) {
+            ar[i+ ar1.length+1] = ar2[i];
+        }
+        print(ar);
+    }
+    void print(T[] ar)
+    {
+        System.out.println(Arrays.toString(ar));
+    }
+
+}
+class test
+{
+    public static void main(String[] args) {
+        Array_Operation<Integer> obj = new Array_Operation<Integer>();
+        Integer ar1[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        Integer ar2[] = {12,34,54,24,65,23};
+        obj.mergeArray(ar1,ar2);
+  //      obj.print(ar);
     }
 }
